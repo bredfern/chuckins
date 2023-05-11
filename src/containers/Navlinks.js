39ffa8c0fdc8bc2dbb0aca2f6edaf2e7e7a76b80
Brian Redfern <brianwredfern@gmail.com>
2023-05-11 15:20:17 -0700

    updated

--- src/containers/Navlinks.js
+++ src/containers/Navlinks.js
@@ -4,11 +4,11 @@ import Link from 'next/link'
 function Navlinks(){
     return(
         <>
-            <li className="mr-2"><Link href="/about-us">About</Link></li>
+            <li className="mr-2"><Link href="/about">About</Link></li>
             <li className="mr-2"><Link href="/products">Products</Link></li>
             <li className="mr-2"><Link href="/quote">Instant Quote</Link></li>
-            <li className="mr-2"><Link href="/jobs">Jobs</Link></li>
-            <li className="mr-2"><Link href="/contact-us">Contact Us</Link></li>
+            <li className="mr-2"><Link href="/careers">Careers</Link></li>
+            <li className="mr-2"><Link href="/contact">Contact</Link></li>
         </>
     )
 }

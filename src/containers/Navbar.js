b824d07278da4186fbf9481e0949823f5835d670
Brian Redfern <brianwredfern@gmail.com>
2023-06-08 16:09:01 -0700

    update

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 function Navbar(){
     return(
-        <div className="w-full flex justify-center  shadow-md  text-primary-content bg-primary bg-black">
+        <div className="w-full flex justify-center  shadow-md  text-primary-content bg-black">
           <div className="navbar bg-black  max-w-5xl">
             <div className="flex-none lg:hidden">
               <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">

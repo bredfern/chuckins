55eff3059e86ec334f7c79065efc3839d97d0434
Brian Redfern <brianwredfern@gmail.com>
2023-06-09 20:29:42 -0700

    update

---
+++ src/components/common/Careers.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+
+function Features(){
+    return(
+        <>
+          <div className="grid place-items-center w-full bg-base-100">
+            <div className="max-w-5xl py-24 content-center justify-center">
+              <div className="card">
+                <div className="card-body">
+                  <h2 className="card-title">Careers</h2>
+                  <p>
+                    Agent looking for a new team?
+                    Looking to get licensed and break into the industry?
+                    Call us to find out more. 424-298-9612
+                  </p>
+                  <p>
+                   <Link href="/consultation" className="btn btn-primary bg-black">Schedule an Interview</Link>
+                  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </>
+    )
+}
+
+export default Features
--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,15 +1,13 @@
 import InnerPageContainer from "@/components/common/InnerPageContainer";
 import Head from 'next/head'
-
+import Careers from '@/components/common/Careers'
 export default function Page() {
     return (
       <InnerPageContainer title="Careers">
         <Head>
             <title> CJM Life - Sell with Us!</title>
         </Head>
-        <p>
-          Agent looking for a new team?  Looking to get licensed and break into the industry? Call us to find out more. 424-298-9612
-        </p>
+       <Careers />
       </InnerPageContainer>
     )
   }
